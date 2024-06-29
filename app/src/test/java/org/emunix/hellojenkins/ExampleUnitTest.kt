bf6b91efba99137c8f278e90d94cac82f298bdaa
Boris Timofeev <mashin87@gmail.com>
2024-06-29 15:10:13 +0300

    to test Detekt check fails

--- app/src/test/java/org/emunix/hellojenkins/ExampleUnitTest.kt
+++ app/src/test/java/org/emunix/hellojenkins/ExampleUnitTest.kt
@@ -2,7 +2,7 @@ package org.emunix.hellojenkins
 
 import org.junit.Test
 
-import org.junit.Assert.assertEquals
+import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
